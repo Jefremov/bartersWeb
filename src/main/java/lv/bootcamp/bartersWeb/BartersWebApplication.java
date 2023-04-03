49637a252a9d59861dbdfa1fdef7d8b3d995d8ee
jefremov <konstantin.jefremov@gmail.com>
2023-04-03 15:35:07 +0300

    spring boot init

---
+++ src/main/java/lv/bootcamp/bartersWeb/BartersWebApplication.java
@@ -0,0 +1,13 @@
+package lv.bootcamp.bartersWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BartersWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BartersWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lv/bootcamp/bartersWeb/BartersWebApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.bootcamp.bartersWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BartersWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
