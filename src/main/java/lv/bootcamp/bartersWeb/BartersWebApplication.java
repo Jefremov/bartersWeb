4103b9c9bac92ed2c73eae834bab34a4882d950e
jefremov <konstantin.jefremov@gmail.com>
2023-04-04 13:13:32 +0300

    access test

--- src/main/java/lv/bootcamp/bartersWeb/BartersWebApplication.java
+++ src/main/java/lv/bootcamp/bartersWeb/BartersWebApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BartersWebApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("Hello world!");
 		SpringApplication.run(BartersWebApplication.class, args);
 	}
 
