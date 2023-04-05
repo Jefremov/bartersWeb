b1edd1ca819b9cabe85be920a0419acd78c012d6
Berta <info.berta@gmail.com>
2023-04-05 19:28:55 +0300

    add enum, remove constructor

--- src/main/java/lv/bootcamp/bartersWeb/entity/Trade.java
+++ src/main/java/lv/bootcamp/bartersWeb/entity/Trade.java
@@ -2,41 +2,49 @@ package lv.bootcamp.bartersWeb.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import java.util.Date;
 
 @Entity
 @Data
 @Table(name = "trades")
-@Getter
-@Setter
 public class Trade {
+
+    private enum TradeStatus {
+        PENDING("Pending"),
+        ACCEPTED("Accepted"),
+        REJECTED("Rejected");
+
+        private final String displayName;
+
+        TradeStatus(String displayName) {
+            this.displayName = displayName;
+        }
+
+        public String getDisplayName() {
+            return displayName;
+        }
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "tradeId")
     private Long id;
+
     @OneToOne
+    @JoinColumn(name = "itemId")
     private Item item;
+
     @Column(name = "offeredItemId")
     private Long offeredItemId;
+
+    @Enumerated(EnumType.STRING)
     @Column(name = "status")
-    private String status;
+    private TradeStatus status;
+
     @Column(name = "comment")
     private String comment;
+
     @Column(name = "date")
     private Date date;
 
-    public Trade(Long id, Item item, Long offeredItemId, String status, String comment, Date date) {
-        this.id = id;
-        this.item = item;
-        this.offeredItemId = offeredItemId;
-        this.status = status;
-        this.comment = comment;
-        this.date = date;
-    }
-
-    public Trade() {
-    }
 }
