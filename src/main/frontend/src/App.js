b83ba6d7062218feacfa4dbc9f478bd1df9018ce
Elizabete Titane <etitane28@gmail.com>
2023-04-11 19:24:23 +0300

    added router

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
-import TestComponent from './components/TestComponent';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/HomePage';
+import Items from './pages/ItemsPage';
+import Trades from './pages/TradesPage';
+import Layout from './components/Navigation';
+import TestComponent from './pages/TestComponent';
 
 function App() {
   return (
-    <div className="App">
-        <TestComponent />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/items" element={<Items />} />
+            <Route path="/trades" element={<Trades />} />
+            <Route path="/test" element={<TestComponent />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/main/frontend/src/pages/ItemsPage.js
@@ -0,0 +1,5 @@
+const Items = () => {
+  return <h1>Items</h1>;
+};
+
+export default Items;
