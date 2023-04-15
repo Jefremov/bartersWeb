dc328604e96910cc7be0f206fd50d24179836ee8
Berta <info.berta@gmail.com>
2023-04-15 23:56:23 +0300

    implement Trade Create form and request submit in the UI

--- src/main/frontend/src/components/ItemCard.js
+++ src/main/frontend/src/components/ItemCard.js
@@ -1,9 +1,17 @@
 import React, { useState, memo } from 'react';
 import { Paper, Typography, Button, Modal, Box, Divider } from '@mui/material';
+import FormControl from '@mui/material/FormControl';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import Textarea from '@mui/material/TextareaAutosize';
+import axios from 'axios';
 
 const ItemCard = ({ items }) => {
   const [showMoreInfoDialog, setShowMoreInfoDialog] = useState(false);
+  const [showCreateTradeModal, setShowCreateTradeModal] = useState(false);
   const [selectedItem, setSelectedItem] = useState(null);
+  const [offeredItemId, setOfferedItemId] = React.useState('');
+  const [text, setText] = React.useState('');
 
   const handleShowMoreInfoClick = (itemObj) => {
     setSelectedItem(itemObj);
@@ -13,6 +21,42 @@ const ItemCard = ({ items }) => {
   const handleShowMoreInfoClose = () => {
     setShowMoreInfoDialog(false);
   };
+
+  const handleCreateTradeClick = (itemObj) => {
+    setSelectedItem(itemObj);
+    setShowCreateTradeModal(true);
+  };
+
+  const handleCreateTradeClose = () => {
+    setShowCreateTradeModal(false);
+  };
+
+  const handleCreateTradeFormSubmit = () => {
+    const config = {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    };
+  
+    axios.post('api/trades/create', {
+      itemId: selectedItem.id,
+      offeredItemId: offeredItemId,
+      text: text
+    }, config)
+    .then(response => {
+      console.log('Trade created successfully:', response.data);
+      // add success popup
+      handleCreateTradeClose();
+    })
+    .catch(error => {
+      console.error('Error creating trade:', error);
+    });
+  
+    console.log('offeredItemId', offeredItemId);
+    console.log('selected item', selectedItem.id);
+    console.log('text', text);
+  };
+  
 
   return (
     <>
@@ -31,14 +75,73 @@ const ItemCard = ({ items }) => {
 
 
         <div style={{display: 'flex', justifyContent: 'space-between'}}>
-        <Button size="small" variant="outlined" color="error" onClick={() => handleShowMoreInfoClick(itemObj)}>
-          Show More Info
+
+        <Button size="small" variant="outlined" color="info" onClick={() => handleShowMoreInfoClick(itemObj)}>
+          DETAILS
         </Button>
+
+        <Button size="small" variant="contained" color="info" onClick={() => handleCreateTradeClick(itemObj)}>
+          TRADE
+        </Button>
+
         </div>
 
+        {/* CREATE TRADE MODAL*/}  
+
+        {selectedItem && selectedItem.id === itemObj.id && (
+          <Modal
+            open={showCreateTradeModal}
+            onClose={handleCreateTradeClose}
+          >
+          <Box sx={{ position: 'absolute', top: '10%', left: '50%', transform: 'translate(-50%, -10%)', width: 400, bgcolor: 'background.paper', boxShadow: 24, p: 4 }}>
+            <Typography variant="h6" gutterBottom>
+              Create Trade
+            </Typography>
+
+            <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+            <section>
+                <b>{itemObj.title}</b>
+                <img src={itemObj.image} alt={itemObj.title} style={{width: '100%'}} />
+              </section>
+
+              <h3>TRADE FOR</h3>
+              <div>OFFERED ITEM: </div>
+              <Select
+                labelId="demo-select-small"
+                id="demo-select-small"
+                value={offeredItemId}
+                label="Offered item"
+                onChange={(event) => setOfferedItemId(event.target.value)}
+              >
+                {items.map((item) => (
+                  <MenuItem value={item.id}>{item.title}</MenuItem>
+                ))}
+              </Select>
+
+              <br/>
+              <h6>MESSAGE:</h6>
+              <Textarea
+                placeholder="Type in here…"
+                defaultValue="..."
+                minRows={6}
+                maxRows={6}
+                onChange={(event) => setText(event.target.value)}
+                title='Comment'
+              />
+
+            </FormControl>
+
+              <div style={{width:"100%", textAlign: "right", marginTop: '6px'}}>
+                <Button size="small" variant="contained" onClick={handleCreateTradeFormSubmit}>SUBMIT</Button>
+                <Button size="small" variant="contained" onClick={handleCreateTradeClose}>CANCEL</Button>
+              </div>
+            </Box>
+          </Modal>
+        )}
 
 
         {/* ITEM DETAILS MODAL */}
+
         {selectedItem && selectedItem.id === itemObj.id && (           
           <Modal
             open={showMoreInfoDialog}
