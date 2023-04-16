08160fbc6564dae88ab3691522625201342bacf1
Berta <info.berta@gmail.com>
2023-04-16 12:51:16 +0300

    add Item filtering UI

--- src/main/frontend/src/pages/ItemsPage.js
+++ src/main/frontend/src/pages/ItemsPage.js
@@ -2,25 +2,40 @@ import React, { useEffect, useState } from 'react';
 import ItemCard from '../components/ItemCard';
 import { Button } from '@mui/material';
 import AddItemForm from '../components/AddItemForm';
+import ECategory from '../enums/ECategory';
 
 const Items = () => {
   const [allItems, setAllItems] = useState([]);
   const [showModal, setShowModal] = useState(false);
+  const [categoryFilter, setCategoryFilter] = useState(null);
 
-  const handleShowModalClick = (itemObj) => {
+  const handleShowModalClick = () => {
     setShowModal(true);
+  };
+
+  const handleCategoryFilter = (category) => {
+    setCategoryFilter(category);
   };
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch('/api/items');
-      const json = await response.json();
-      setAllItems(json);
+      const url = categoryFilter ? `/api/items/category/${categoryFilter}` : '/api/items';
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const json = await response.json();
+        setAllItems(json);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setAllItems([]);
+      }
     };
     fetchData();
-  }, []);
+  }, [categoryFilter]);
 
-  console.log('Items', allItems);
+  console.log(allItems);
 
   return (
     <>
@@ -31,11 +46,26 @@ const Items = () => {
         <AddItemForm />
       )}
       <br/>
+      {Object.values(ECategory).map(category => (
+        <Button key={category} style={{marginRight: '10px', marginBottom: '10px'}} variant={categoryFilter === category ? "contained" : "outlined"} onClick={() => handleCategoryFilter(category.value)}>{category.displayName}</Button>
+      ))}
     </div>
+  
+    {allItems?.length > 0 &&
       <div style={{ display: 'flex', flexWrap: 'wrap' }}>
         <ItemCard items={allItems} />
       </div>
+    }
 
+    {allItems?.length === 0 && (
+      <div style={{textAlign: 'center', color: 'gray', marginTop: '20px'}}>
+        No items in this category, yet.
+        <br/><br/>
+        Would you like to add one? 
+        <br/><br/>
+        <Button style={{marginBottom: '10px'}} variant="contained" color="primary" onClick={handleShowModalClick}>Create Item</Button>
+      </div>
+    )}
     </>
   );
 }
