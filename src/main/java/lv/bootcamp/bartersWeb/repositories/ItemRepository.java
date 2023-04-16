234cf8426c13dc05ae5dfd2c54d0ececcfe316fd
Ralfs Laksa <ralfslaksa03@gmail.com>
2023-04-16 11:37:06 +0300

    Item,trade integration

--- src/main/java/lv/bootcamp/bartersWeb/repositories/ItemRepository.java
+++ src/main/java/lv/bootcamp/bartersWeb/repositories/ItemRepository.java
@@ -1,7 +1,13 @@
 package lv.bootcamp.bartersWeb.repositories;
 
+import lv.bootcamp.bartersWeb.entities.EItemStatus;
 import lv.bootcamp.bartersWeb.entities.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findAllByStatus(EItemStatus status);
+
 }
