681bf1b30eb32dd205b41c5683c3a44b405ac5c4
jefremov <konstantin.jefremov@gmail.com>
2023-04-16 11:16:37 +0300

    enable spring doc

--- src/main/java/lv/bootcamp/bartersWeb/authService/RegisterRequest.java
+++ src/main/java/lv/bootcamp/bartersWeb/authService/RegisterRequest.java
@@ -1,5 +1,6 @@
 package lv.bootcamp.bartersWeb.authService;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -13,15 +14,18 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "User registration form")
 public class RegisterRequest {
 
     @NotBlank
+    @Schema(description = "Uniq username")
     private String username;
     @NotBlank
     @Size(min = 8, message = "Password must have at least 8 characters")
     private String password;
     @NotBlank
     @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "Incorrect email")
+    @Schema(description = "Uniq email")
     private String email;
     @NotNull
     private String phoneNumber;
